b663531f447add0b3a9a0a1c38e8941f6e991e9d
wangboy91 <wangboy91@gmail.com>
2024-11-20 19:32:44 +0800

    增加demo

---
+++ MediatRDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MediatR;
+using MediatRDemo;
+using Microsoft.Extensions.DependencyInjection;
+
+
+Console.WriteLine("Hello, World!");
+var services = new ServiceCollection();
+services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+services.AddSingleton<IPushDemo, PushDemo>();
+var serviceProvider = services.BuildServiceProvider();
+var pushDemo = serviceProvider.GetService<IPushDemo>();
+pushDemo?.PushData();
---
+++ MediatRDemo/PushDemo.cs
@@ -0,0 +1,42 @@
+using MediatR;
+
+namespace MediatRDemo;
+
+public class SendEvent : INotification
+{
+    public string Type { get; set; }
+}
+
+public interface IPushDemo
+{
+    void PushData();
+}
+public class PushDemo:IPushDemo
+{
+    private readonly IMediator _mediator;
+
+    public PushDemo(IMediator mediator)
+    {
+        _mediator= mediator;
+    }
+    public void PushData()
+    {
+        _mediator.Publish(new SendEvent() {Type = "测试一下"});
+    }
+}
+public class EventHandle1 : INotificationHandler<SendEvent>
+{
+    public Task Handle(SendEvent notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"EventHandle1收到新消息:{notification.Type} 登录成功了");
+        return Task.CompletedTask;
+    }
+}
+public class EventHandle2 : INotificationHandler<SendEvent>
+{
+    public Task Handle(SendEvent notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"EventHandle1收到新消息:{notification.Type} 登录成功了");
+        return Task.CompletedTask;
+    }
+}
